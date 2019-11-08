bdd499e5c2d04bcdc380948819393131a0901b3b
Ben Ritter <benjaminaritter@gmail.com>
2019-11-08 12:59:43 +0800

    Revert to old-style imports in configure-android.js

--- src/android/hooks/configure-android.js
+++ src/android/hooks/configure-android.js
@@ -7,14 +7,16 @@ module.exports = function (ctx) {
     if (ctx.opts.platforms.indexOf('android') < 0) {
         return;
     }
-    /*var fs = ctx.requireCordovaModule('fs'),
+    var fs = ctx.requireCordovaModule('fs'),
         path = ctx.requireCordovaModule('path'),
-        deferral = ctx.requireCordovaModule('q').defer(); substitute with require to be compatible with cordova 9.0.0*/
+        deferral = ctx.requireCordovaModule('q').defer();
 	
+  /*
 	var fs = require('fs'),
         path = require('path'),
 		Q = require('q');
 	var deferral = new Q.defer();
+  */
 
     function replace_string_in_file(filename, to_replace, replace_with) {
         var data = fs.readFileSync(filename, 'utf8');
